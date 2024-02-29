81f8f9d2fdee6e09e09c8df6decf0c9166a3f229
karthickelango <karthik5ive@gmail.com>
2024-02-29 15:41:10 +0530

    add foldr

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,6 +12,9 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link>
+      <Link href={'/(auth)/sign_in'} asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 };
